c941dadbff1a3efc1f53ce7d44831ca7d6e04766
DevNovaes <143047178+euvnovaes@users.noreply.github.com>
2024-04-10 12:54:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import ConteudoEsquerda from './components/conteudoEsquerda';
+
+function App() {
+  return (
+    <>
+      <ConteudoEsquerda />
+
+      <div className="conteudoCentro">
+
+      </div>
+
+      <div className="conteudoDireita">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/conteudoEsquerda.js
@@ -0,0 +1,16 @@
+import logo from '../assets/images/LogoInsta.png'
+
+export default function ConteudoEsquerda() {
+    return (
+        <>
+            <div className="conteudoEsquerda">
+                <div className="logoInstagram">
+                    <img src={logo} alt="Logo Instagram" />
+                </div>
+                <div className="menu">
+                    <div className=
+                </div>
+            </div>
+        </>
+    )
+}
